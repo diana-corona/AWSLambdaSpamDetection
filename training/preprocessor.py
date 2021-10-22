ca1a5b66228e2f80d9f34bd55cef096acacdf96f
Diana Corona <diana.corona.g@gmail.com>
2021-10-21 21:13:19 -0500

    remove: training folder to move to different repository

--- training/preprocessor.py
+++
@@ -1,7 +0,0 @@
-import re
-
-def preprocessor(text):
-    text = re.sub('<[^>]*>', '', text) # Effectively removes HTML markup tags
-    emoticons = re.findall('(?::|;|=)(?:-)?(?:\)|\(|D|P)', text)
-    text = re.sub('[\W]+', ' ', text.lower()) + ' '.join(emoticons).replace('-', '')
-    return text
